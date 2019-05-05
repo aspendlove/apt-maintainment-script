78f408cb71f7a19b25d845d50bf974526c8f1a6c
copperly123 <aidancspendlove@gmail.com>
2019-05-05 13:23:24 -0600

    fix install.sh

--- install.sh
+++ install.sh
@@ -20,12 +20,12 @@ sudo cp maintain /usr/local/bin
 echo '
 '
 #check if the restart flag is set
-if [ restart != 1 ]
+if [ $restart != 1 ]
 then
 	#if not then tell the user that the program is installed
-	echo "fully installed"
+	echo "Fully installed"
 else
 	#if so then tell the user that they will need to restart
-	echo "you will need to reboot to fully install"
+	echo "You will need to restart to finish the installation"
 fi
 exit
